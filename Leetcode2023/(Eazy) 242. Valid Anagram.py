c6e6bf974a5d7893b23704a13b64a9172199b99e
Jseastar <jseastar92@gmail.com>
2023-10-25 22:21:00 +0900

    leetcode_1025

--- Leetcode2023/(Eazy) 242. Valid Anagram.py
+++ Leetcode2023/(Eazy) 242. Valid Anagram.py
@@ -1,18 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-
         sd, td = dict(), dict()    
         for c in s[:]:
             sd.setdefault(c, 0)
-            print(c)
             sd[c] += 1
-        print(sd)    
+
         for c in t[:]:
             td.setdefault(c, 0)
-            print(c)
             td[c] += 1
-        print(td)
+
         return True if sd==td else False
 
 
